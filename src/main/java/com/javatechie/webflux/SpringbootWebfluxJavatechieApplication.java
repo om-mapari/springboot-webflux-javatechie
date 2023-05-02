5173f10ffad9a20eebe67600288e73f70d7ec9ef
om mapari <mapariom05@gmail.com>
2023-05-02 14:49:32 +0530

    first commit

---
+++ src/main/java/com/javatechie/webflux/SpringbootWebfluxJavatechieApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxJavatechieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxJavatechieApplication.class, args);
+	}
+
+}
